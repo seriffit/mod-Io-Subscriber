79b65547c14ae74c4a96ab2497c7463c7de5a3a5
ottoh-commits <55714600+ottoh-commits@users.noreply.github.com>
2023-07-05 18:13:26 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+We are huge fans of Ready or Not game! However the game uses Mod.Io for mod support. We didn't find any way to easily subsribe to all your friends mods, so we did this ugly thing.
+
+This uses your own Oauth token and your friends Oauth token. Your friends Oauth is used for fetching all the mods they have subscribed and your own Oauth is used to subscribe it to your own account.
+
+After clicking the button, you can double check from mod.io that all your friends subscribed mods have been subscribed to you also.
+
+Browsers console also gives you more information, in case of any errors!
+
+You and your friend both need to create your own Oauth Token in the Mod.Io.
+<b>DO NOT SHARE YOUR OAUTH TOKEN TO ANYONE YOU CAN'T TRUST</b>
---
+++ getMods.js
@@ -0,0 +1,58 @@
+"use strict"
+const acceptHeader = "application/json";
+
+async function getModsInJsonData(url, options) {
+  const response = await fetch(url, options);
+  const jsonResponse = await response.json();
+  console.log("Yhteensä tämän verran modeja: " + jsonResponse.result_total);
+  return jsonResponse;
+}
+
+async function subscribeToIds(arrayOfIds, options, gameId) {
+  arrayOfIds.forEach(function (id) {
+    subscribeToId(id, options, gameId);
+  });
+}
+
+async function subscribeToId(id, options, gameId) {
+  const subUrl = `https://api.mod.io/v1/games/${gameId}/mods/${id}/subscribe`;
+  console.log("URL: " + subUrl);
+  const response = await fetch(subUrl, options);
+  console.log(response.status);
+  const responseJSON = await response.json();
+  console.log(responseJSON);
+}
+
+///Starts here
+async function subscibeToFriendsMods() {
+  const subBearerToken = document.getElementById("yourInput").value;
+  const bearerToken = document.getElementById("friendInput").value;
+  const gameId = document.getElementById("gameId").value;
+  const getUrl = "https://api.mod.io/v1/me/subscribed";
+  const getOptions = {
+    method: "GET",
+    headers: {
+      Accept: acceptHeader,
+      Authorization: "Bearer " + bearerToken,
+    },
+  };
+
+  const subOptions = {
+    method: "POST",
+    headers: {
+      Accept: acceptHeader,
+      Authorization: "Bearer " + subBearerToken,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  };
+
+  const jsonResponse = await getModsInJsonData(getUrl, getOptions);
+
+  const modIds = jsonResponse.data.map(function (data) {
+    return data.id;
+  });
+
+  subscribeToIds(modIds, subOptions, gameId);
+
+  console.log(modIds);
+}
